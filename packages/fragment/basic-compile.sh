85438f04be46e3ced55bb7aed3372cbbf6797918
Jason Siefken <siefkenj@gmail.com>
2023-06-24 23:14:51 -0400

    Add bash script to compile fragments

---
+++ packages/fragment/basic-compile.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+TMP_DIR=./tmp_fragment_compile
+
+FRAGMENT_FILE_PATH=$1
+# if $FRAGMENT_FILE_PATH is not set, print usage information
+if [ -z "$FRAGMENT_FILE_PATH" ]; then
+  echo "Usage: ./basic-compile.sh <fragment-file-path> [target-dir]"
+  echo "  <fragment-file-path> is the path to the fragment file to compile"
+  echo "  [target-dir] is the directory to save the compiled fragment to. If not set, defaults to $TMP_DIR"
+  exit 1
+fi
+
+TARGET_DIR=$2
+# if $TARGET_DIR is not set, use $TMP_DIR
+TARGET_DIR=${TARGET_DIR:-$TMP_DIR}
+
+IFS='' read -r -d '' PROJECT_PTX <<"EOF"
+<?xml version="1.0" encoding="utf-8"?>
+<project>
+  <targets>
+    <!-- To build this target, run `pretext build web` -->
+    <target name="web">
+      <format>html</format>
+      <source>main.ptx</source>
+      <publication>publication.ptx</publication>
+      <output-dir>output</output-dir>
+    </target>
+  </targets>
+</project>
+EOF
+
+IFS='' read -r -d '' PUBLICATION_PTX <<"EOF"
+<?xml version="1.0" encoding="UTF-8"?>
+<publication>
+	<common>
+		<chunking level="2" />
+		<tableofcontents level="2" />
+		<exercise-inline statement="yes" hint="yes" answer="no" solution="no" />
+		<exercise-divisional statement="yes" hint="yes" />
+		<fillin textstyle="underline" mathstyle="shade" />
+	</common>
+	<source>
+		<directories external="./assets" generated="./generated-assets" />
+	</source>
+	<numbering>
+		<blocks level="2" />
+		<projects level="2" />
+		<equations level="2" />
+		<footnotes level="2" />
+	</numbering>
+	<latex print="no" sides="one" font-size="10" draft="no">
+		<page right-alignment="flush" bottom-alignment="ragged" />
+		<asymptote links="no" />
+		<worksheets formatted="yes" />
+	</latex>
+	<html platform="web">
+		<knowl theorem="no" proof="yes" definition="no" example="yes" example-solution="yes" project="no" task="no" remark="no" objectives="no" outcomes="no" figure="no" table="no" listing="no" list="no" exercise-inline="yes" exercise-divisional="no" exercise-worksheet="no" exercise-readingquestion="no" />
+		<css style="default" colors="blue_red" />
+		<search variant="default" />
+		<calculator model="none" activecode="none" />
+		<webwork inline="dynamic" divisional="static" reading="static" worksheet="static" project="dynamic" />
+	</html>
+</publication>
+EOF
+
+
+IFS='' read -r -d '' ARTICLE_TEMPLATE <<"EOF"
+<?xml version="1.0" encoding="UTF-8" ?>
+<pretext>
+    <article>
+        <FRAGMENT />
+    </article>
+</pretext>
+EOF
+
+MAIN_PTX=$(npx pretext-fragment --fragment-file "$FRAGMENT_FILE_PATH" --template "$ARTICLE_TEMPLATE")
+
+# A hard-coded version of a fragment file. Uncomment to force the use of this file.
+#
+#IFS='' read -r -d '' MAIN_PTX <<"EOF"
+#<?xml version="1.0" encoding="UTF-8"?><pretext xml:id="FRAGMENT_PARENT_ID__2">
+#    <article xml:id="FRAGMENT_PARENT_ID__1">
+#        <chapter xml:id="FRAGMENT_PARENT_ID__0"><p>Some text.</p></chapter>
+#    </article>
+#</pretext>
+#EOF
+
+
+# Make a basic project for the pretext-cli
+mkdir -p $TMP_DIR
+echo "$PUBLICATION_PTX" > $TMP_DIR/publication.ptx
+echo "$PROJECT_PTX" > $TMP_DIR/project.ptx
+echo "$MAIN_PTX" > $TMP_DIR/main.ptx
+
+pushd $TMP_DIR
+pretext build web
+
+# find the rendered fragment and extract it
+# The file we want to extract has the form `FRAGMENT_PARENT_ID__##.html` where ## is the smallest number
+FRAGMENT_FILE=$(find output -name "FRAGMENT_PARENT_ID__*.html" | sort -V | head -n 1)
+
+# get the base name of the fragment file without the extension
+FRAGMENT_BASENAME=$(basename "$FRAGMENT_FILE_PATH")
+FRAGMENT_BASENAME="${FRAGMENT_BASENAME%.*}"
+OUT_FILE="$FRAGMENT_BASENAME.html"
+
+
+echo "Extracting fragment from $FRAGMENT_FILE"
+OUT=$(npx pretext-fragment --extract-from-html-file "$FRAGMENT_FILE" --pretty-print)
+popd > /dev/null
+echo "$OUT" > "$TARGET_DIR/$OUT_FILE"
+echo "Saved fragment to $TARGET_DIR/$OUT_FILE"
