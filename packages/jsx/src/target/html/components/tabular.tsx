434835ce617559bd9f5b62ca19d9eb3639996563
Steven Clontz <steven.clontz@gmail.com>
2023-07-11 22:40:38 +0000

    merge row and column logic into tabular component to use tabular state

--- packages/jsx/src/target/html/components/tabular.tsx
+++ packages/jsx/src/target/html/components/tabular.tsx
@@ -3,50 +3,56 @@ import { PretextStateContext } from "../state";
 import { ReplacerComponent } from "../replacers/replacer-factory";
 import { XastElement } from "@pretext-book/jsx/src/utils/xast";
 
-export const Cell: ReplacerComponent = function ({ node }) {
-    const state = React.useContext(PretextStateContext);
-    return (
-        <td className="b0 l l0 lines m r0 t0">
-            {state.processContent(node.children)}
-        </td>
-    );
-};
-
-export const CellHeader: ReplacerComponent = function ({ node }) {
-    const state = React.useContext(PretextStateContext);
-    return (
-        <th scope="col" className="b0 l l0 lines m r0 t0">
-            {state.processContent(node.children)}
-        </th>
-    );
-};
-
-export const Row: ReplacerComponent = function ({ node }) {
-    const isHeader = node.attributes?.header == "yes"
-    const cells = node.children.filter((n) :n is XastElement=>n.type == "element" && n.name=="cell")
-    if (isHeader) {
-        return (
-            <tr className="header-horizontal">
-                {cells.map(c=>CellHeader({node: c}))}
-            </tr>
-        );
-    } else {
-        return (
-            <tr>
-                {cells.map(c=>Cell({node: c}))}
-            </tr>
-        )
-    }
-};
-
 export const Tabular: ReplacerComponent = function ({ node }) {
-    const rows = node.children.filter((n) :n is XastElement=>n.type == "element" && n.name=="row")
+    const state = React.useContext(PretextStateContext);
+    const rows = node.children.filter(
+        (n): n is XastElement => n.type == "element" && n.name == "row"
+    );
+    const cols = node.children.filter(
+        (n): n is XastElement => n.type == "element" && n.name == "col"
+    );
 
     return (
         <div className="natural-width tabular-box">
             <table className="tabular">
                 <tbody>
-                    {rows.map(r=>Row({node: r}))}
+                    {rows.map((r) => {
+                        const isHeader = r.attributes?.header == "yes";
+                        const cells = r.children.filter(
+                            (n): n is XastElement =>
+                                n.type == "element" && n.name == "cell"
+                        );
+                        return (
+                            <tr
+                                className={
+                                    isHeader ? "header-horizontal" : undefined
+                                }
+                            >
+                                {cells.map((c) => {
+                                    if (isHeader) {
+                                        return (
+                                            <th
+                                                className="b0 l l0 lines m r0 t0"
+                                                scope="col"
+                                            >
+                                                {state.processContent(
+                                                    c.children
+                                                )}
+                                            </th>
+                                        );
+                                    } else {
+                                        return (
+                                            <td className="b0 l l0 lines m r0 t0">
+                                                {state.processContent(
+                                                    c.children
+                                                )}
+                                            </td>
+                                        );
+                                    }
+                                })}
+                            </tr>
+                        );
+                    })}
                 </tbody>
             </table>
         </div>
