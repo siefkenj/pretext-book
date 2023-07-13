d1dcc110a8ab8cbb17c5c46fcdfc0678b7c9dacb
Steven Clontz <steven.clontz@gmail.com>
2023-07-13 03:55:58 +0000

    refactored

--- packages/jsx/src/target/html/components/tabular.tsx
+++ packages/jsx/src/target/html/components/tabular.tsx
@@ -3,144 +3,147 @@ import { PretextStateContext } from "../state";
 import { ReplacerComponent } from "../replacers/replacer-factory";
 import { XastElement } from "@pretext-book/jsx/src/utils/xast";
 
-const cellClasses = function (
+const weightNum = { major: "3", medium: "2", minor: "1", none: "0" };
+const weightAttrNum = (s: string | null | undefined) => {
+    if (s && s in weightNum) {
+        return weightNum[s as keyof typeof weightNum];
+    }
+    return "0"; // default none
+};
+
+const vAlignAbbr = { top: "t", bottom: "b", middle: "m" };
+const vAlignAttrAbbr = (s: string | null | undefined) => {
+    if (s && s in vAlignAbbr) {
+        return vAlignAbbr[s as keyof typeof vAlignAbbr];
+    }
+    return "m"; // default middle
+};
+
+const hAlignAbbr = { center: "c", right: "r", justify: "j", left: "l" };
+const hAlignAttrAbbr = (s: string | null | undefined) => {
+    if (s && s in hAlignAbbr) {
+        return hAlignAbbr[s as keyof typeof hAlignAbbr];
+    }
+    return "l"; // default left
+};
+
+const cellClassName = function (
     tabular: XastElement,
-    r_index: number,
-    c_index: number
+    row_index: number,
+    col_index: number
 ): string {
     const rows = tabular.children.filter(
         (n): n is XastElement => n.type == "element" && n.name == "row"
     );
+    const row: XastElement = rows[row_index];
+
+    // col may be undefined
     const cols = tabular.children.filter(
         (n): n is XastElement => n.type == "element" && n.name == "col"
     );
-    const row: XastElement = rows[r_index];
+    const col: XastElement | undefined = cols[col_index];
+
     const cells = row.children.filter(
         (n): n is XastElement => n.type == "element" && n.name == "cell"
     );
-    const cell: XastElement = cells[c_index];
-    const col: XastElement | undefined = cols[c_index];
+    const cell: XastElement = cells[col_index];
 
-    const toNum = (s: string | null | undefined) => {
-        if (s == "major") {
-            return "3";
-        }
-        if (s == "medium") {
-            return "2";
-        }
-        if (s == "minor") {
-            return "1";
-        }
-        return "0";
+    let lines = {
+        // defaults
+        bottom: weightAttrNum(null),
+        top: weightAttrNum(null),
+        left: weightAttrNum(null),
+        right: weightAttrNum(null),
     };
 
-    let bottomLine: string = "b0";
-    if (tabular.attributes?.bottom && r_index == rows.length - 1) {
-        bottomLine = "b" + toNum(tabular.attributes.bottom);
+    // Weights are assigned in increasing priority: tabular, col, row, then cell
+    // Commented out sections below are for non-schema-compliant attributes
+
+    if (tabular.attributes?.bottom && row_index == rows.length - 1) {
+        lines.bottom = weightAttrNum(tabular.attributes.bottom);
     }
-    // if (col?.attributes?.bottom && r_index == rows.length - 1) {
-    //     bottomLine = "b" + toNum(col.attributes.bottom);
+    // if (col?.attributes?.bottom && row_index == rows.length - 1) {
+    //     lines.bottom = weightAttrNum(col.attributes.bottom);
     // }
     if (row.attributes?.bottom) {
-        bottomLine = "b" + toNum(row.attributes.bottom);
+        lines.bottom = weightAttrNum(row.attributes.bottom);
     }
     if (cell.attributes?.bottom) {
-        bottomLine = "b" + toNum(cell.attributes.bottom);
+        lines.bottom = weightAttrNum(cell.attributes.bottom);
     }
 
-    let topLine: string = "t0";
-    if (tabular.attributes?.top && r_index == 0) {
-        topLine = "t" + toNum(tabular.attributes.top);
+    if (tabular.attributes?.top && row_index == 0) {
+        lines.top = weightAttrNum(tabular.attributes.top);
     }
-    if (col?.attributes?.top && r_index == 0) {
-        topLine = "t" + toNum(col.attributes.top);
+    if (col?.attributes?.top && row_index == 0) {
+        lines.top = weightAttrNum(col.attributes.top);
     }
     // if (row.attributes?.top) {
-    //     topLine = "t" + toNum(row.attributes.top);
+    //     lines.top = weightAttrNum(row.attributes.top);
     // }
     // if (cell.attributes?.top) {
-    //     topLine = "t" + toNum(cell.attributes.top);
+    //     lines.top = weightAttrNum(cell.attributes.top);
     // }
 
-    let leftLine: string = "l0";
-    if (tabular.attributes?.left && c_index == 0) {
-        leftLine = "l" + toNum(tabular.attributes.left);
+    if (tabular.attributes?.left && col_index == 0) {
+        lines.left = weightAttrNum(tabular.attributes.left);
     }
     // if (col?.attributes?.left) {
-    //     leftLine = "l" + toNum(col.attributes.left);
+    //     lines.left = weightAttrNum(col.attributes.left);
     // }
-    if (row.attributes?.left && c_index == 0) {
-        leftLine = "l" + toNum(row.attributes.left);
+    if (row.attributes?.left && col_index == 0) {
+        lines.left = weightAttrNum(row.attributes.left);
     }
     // if (cell.attributes?.left) {
-    //     leftLine = "l" + toNum(cell.attributes.left);
+    //     lines.left = weightAttrNum(cell.attributes.left);
     // }
 
-    let rightLine: string = "r0";
-    if (tabular.attributes?.right && c_index == cells.length - 1) {
-        rightLine = "r" + toNum(tabular.attributes.right);
+    if (tabular.attributes?.right && col_index == cells.length - 1) {
+        lines.right = weightAttrNum(tabular.attributes.right);
     }
     if (col?.attributes?.right) {
-        rightLine = "r" + toNum(col.attributes.right);
+        lines.right = weightAttrNum(col.attributes.right);
     }
-    // if (row.attributes?.right && c_index == cells.length - 1) {
-    //     rightLine = "r" + toNum(row.attributes.right);
+    // if (row.attributes?.right && col_index == cells.length - 1) {
+    //     lines.right = weightAttrNum(row.attributes.right);
     // }
     if (cell.attributes?.right) {
-        rightLine = "r" + toNum(cell.attributes.right);
+        lines.right = weightAttrNum(cell.attributes.right);
     }
 
-    const toVAlign = (s: string | null | undefined) => {
-        if (s == "top") {
-            return "t";
-        }
-        if (s == "bottom") {
-            return "b";
-        }
-        return "m"; // middle
+    let align = {
+        // defaults
+        vertical: vAlignAttrAbbr(null),
+        horizontal: hAlignAttrAbbr(null),
     };
-    let vAlign: string = "m";
+
     if (tabular.attributes?.valign) {
-        vAlign = toVAlign(tabular.attributes.valign);
+        align.vertical = vAlignAttrAbbr(tabular.attributes.valign);
     }
     // if (col?.attributes?.halign) {
-    //     vAlign = toVAlign(col.attributes.valign);
+    //     align.vertical = vAlignAttrAbbr(col.attributes.valign);
     // }
     if (row?.attributes?.halign) {
-        vAlign = toVAlign(row.attributes.valign);
+        align.vertical = vAlignAttrAbbr(row.attributes.valign);
     }
     // if (cell?.attributes?.halign) {
-    //     vAlign = toVAlign(cell.attributes.valign);
+    //     align.vertical = vAlignAttrAbbr(cell.attributes.valign);
     // }
 
-    const toHAlign = (s: string | null | undefined) => {
-        if (s == "center") {
-            return "c";
-        }
-        if (s == "right") {
-            return "r";
-        }
-        if (s == "justify") {
-            return "j";
-        }
-        return "l"; // left
-    };
-
-    let hAlign: string = "l";
     if (tabular.attributes?.halign) {
-        hAlign = toHAlign(tabular.attributes.halign);
+        align.horizontal = hAlignAttrAbbr(tabular.attributes.halign);
     }
     if (col?.attributes?.halign) {
-        hAlign = toHAlign(col.attributes.halign);
+        align.horizontal = hAlignAttrAbbr(col.attributes.halign);
     }
-    if (row?.attributes?.halign) {
-        hAlign = toHAlign(row.attributes.halign);
+    if (row.attributes?.halign) {
+        align.horizontal = hAlignAttrAbbr(row.attributes.halign);
     }
-    if (cell?.attributes?.halign) {
-        hAlign = toHAlign(cell.attributes.halign);
+    if (cell.attributes?.halign) {
+        align.horizontal = hAlignAttrAbbr(cell.attributes.halign);
     }
 
-    return `lines ${vAlign} ${hAlign} ${bottomLine} ${topLine} ${leftLine} ${rightLine}`;
+    return `lines b${lines.bottom} t${lines.top} l${lines.left} r${lines.right} ${align.vertical} ${align.horizontal}`;
 };
 
 export const Tabular: ReplacerComponent = function ({ node }) {
@@ -153,9 +156,13 @@ export const Tabular: ReplacerComponent = function ({ node }) {
         <div className="natural-width tabular-box">
             <table className="tabular">
                 <tbody>
-                    {rows.map((r, r_index) => {
-                        const isHeader = r.attributes?.header == "yes";
-                        const cells = r.children.filter(
+                    {rows.map((row, row_index) => {
+                        const isHeader = row.attributes?.header == "yes";
+                        // CellTag is `th` or `td`
+                        const CellTag = `t${
+                            isHeader ? "h" : "d"
+                        }` as keyof JSX.IntrinsicElements;
+                        const cells = row.children.filter(
                             (n): n is XastElement =>
                                 n.type == "element" && n.name == "cell"
                         );
@@ -165,37 +172,19 @@ export const Tabular: ReplacerComponent = function ({ node }) {
                                     isHeader ? "header-horizontal" : undefined
                                 }
                             >
-                                {cells.map((c, c_index) => {
-                                    if (isHeader) {
-                                        return (
-                                            <th
-                                                className={cellClasses(
-                                                    node,
-                                                    r_index,
-                                                    c_index
-                                                )}
-                                                scope="col"
-                                            >
-                                                {state.processContent(
-                                                    c.children
-                                                )}
-                                            </th>
-                                        );
-                                    } else {
-                                        return (
-                                            <td
-                                                className={cellClasses(
-                                                    node,
-                                                    r_index,
-                                                    c_index
-                                                )}
-                                            >
-                                                {state.processContent(
-                                                    c.children
-                                                )}
-                                            </td>
-                                        );
-                                    }
+                                {cells.map((cell, col_index) => {
+                                    return (
+                                        <CellTag
+                                            className={cellClassName(
+                                                node,
+                                                row_index,
+                                                col_index
+                                            )}
+                                            scope={isHeader ? "col" : undefined}
+                                        >
+                                            {state.processContent(cell.children)}
+                                        </CellTag>
+                                    );
                                 })}
                             </tr>
                         );
