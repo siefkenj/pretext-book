03d467107fdf51ebf345568042837246a12459be
Jason Siefken <siefkenj@gmail.com>
2023-07-19 16:38:06 -0400

    Remove unused code from url.tsx

--- packages/jsx/src/target/html/components/url.tsx
+++ packages/jsx/src/target/html/components/url.tsx
@@ -3,36 +3,6 @@ import { toString } from "xast-util-to-string";
 import { ReplacerComponent } from "../replacers/replacer-factory";
 import { PretextStateContext } from "../state";
 
-const SYMBOL_MAP: Record<string, React.ReactNode> = {
-    ampersand: "&",
-    less: "<",
-    greater: ">",
-    dollar: "$",
-    percent: "%",
-    openbrace: "{",
-    closebrace: "}",
-    hash: "#",
-    backslash: "\\",
-    tilde: "~",
-    circumflex: "^",
-    underscore: "_",
-    left: "←",
-    right: "→",
-    up: "↑",
-    down: "↓",
-    shift: "⇧",
-    solidus: "/",
-    plus: "+",
-    minus: "-",
-    times: "×",
-    "left-paren": "(",
-    obelus: "÷",
-    squared: "x^2",
-    inverse: "x^-1",
-    "right-paren": ")",
-    enter: "⮠",
-};
-
 export const Url: ReplacerComponent = function ({ node }) {
     const state = React.useContext(PretextStateContext);
 
