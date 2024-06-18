c0a1afd169f576dc23bde85cf11dba184454ba99
Jason Siefken <siefkenj@gmail.com>
2024-06-18 11:54:16 -0400

    Fix for redeployment

--- packages/docs/next.config.mjs
+++ packages/docs/next.config.mjs
@@ -123,8 +123,8 @@ if (isGithubActions) {
     // trim off `<owner>/`
     const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
 
-    assetPrefix = `/${repo}/`;
-    basePath = `/${repo}`;
+    assetPrefix = `/${repo}/docs/`;
+    basePath = `/${repo}/docs`;
 }
 
 export default withNextra({
