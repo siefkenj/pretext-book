d967dee720c44d79e4cf0b0bcb306079f8df443a
Jason Siefken <siefkenj@gmail.com>
2024-07-25 17:07:01 -0400

    Add ability to override attributes

---
+++ packages/docs/pages/reference/attributes/_meta.tsx
@@ -0,0 +1,14 @@
+export default {
+    component: {
+        title: <code>component</code>,
+    },
+    label: {
+        title: <code>label</code>,
+    },
+    xmlid: {
+        title: <code>xml:id</code>,
+    },
+    xmllang: {
+        title: <code>xml:lang</code>,
+    },
+};
